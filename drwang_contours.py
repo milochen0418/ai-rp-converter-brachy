f9493540191320537cfaa7c00036bf24f7a1a5c4
Milo Chen <milo_chen@mail2000.com.tw>
2019-10-24 21:03:30 +0800

    Add drwang_contours.py to show all number of contours information

---
+++ drwang_contours.py
@@ -0,0 +1,21 @@
+# Try to seperate program into clear verion and useful functions
+import os
+import pydicom
+import numpy as np
+import cv2
+import copy
+import math
+from decimal import Decimal
+import random
+
+print(cv2.__version__)
+
+from IPython.core.display import display, HTML
+import numpy as np
+import pandas as pd
+
+
+
+
+
+print('Hello World')
